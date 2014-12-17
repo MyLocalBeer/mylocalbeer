93fbe8ff1c857f6b57b12c86b15678b97aec50a2
Liz Cole <liz.cole1227@gmail.com>
2014-12-17 15:21:28 -0600

    Commented out jquery that isnt working on master.blade and correspondng CSS

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -55,28 +55,28 @@
 @stop
 
 @section('bottomscript')
-	<script type="text/javascript">
-		var start_position = $('#thehow').position().top,
-		end_position = null;
+	 <script type="text/javascript">
+	// 	var start_position = $('#thehow').position().top,
+	// 	end_position = null;
 
-		// console.log(start_position);
+	// 	// console.log(start_position);
 
-		$(window).scroll(function() {
-			end_position = $(window).scrollTop();
-			console.log(end_position);
-			if(end_position > 160) {
-				$('#thehow').fadeIn();
-			}
-		});
+	// 	$(window).scroll(function() {
+	// 		end_position = $(window).scrollTop();
+	// 		console.log(end_position);
+	// 		if(end_position > 160) {
+	// 			$('#thehow').fadeIn();
+	// 		}
+	// 	});
 
 //this function is not working correctly. Will scroll the how text but removes the hidden css element and the position fixed hides the photo. Need to find a new css for the "how" photo. 
-		$(window).scroll(function() {
-			end_position = $(window).scrollTop();
-			if(end_position > 620) {
-				$('#how').css('position', 'fixed');
-				$('#thehow').css('overflow-y', 'scroll');
-			}
-		});
+		// $(window).scroll(function() {
+		// 	end_position = $(window).scrollTop();
+		// 	if(end_position > 620) {
+		// 		$('#how').css('position', 'fixed');
+		// 		$('#thehow').css('overflow-y', 'scroll');
+		// 	}
+		// });
 
 // hits mark on y in query attach 2 styles overflow-y: scroll and postion: fixed for the banner
 
