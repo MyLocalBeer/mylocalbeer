67a9e34801aaf6d7b85c0d099a55d10bb4e5bacc
Liz Cole <liz.cole1227@gmail.com>
2015-01-09 09:27:04 -0600

    Working to add a star rating to beer index

--- app/views/beers/index.blade.php
+++ app/views/beers/index.blade.php
@@ -47,11 +47,20 @@
                 </div>
             </article>
         </div>
+        <span class="star-rating">
+            <input type="radio" name="rating" value="1"><i></i>
+            <input type="radio" name="rating" value="2"><i></i>
+            <input type="radio" name="rating" value="3"><i></i>
+            <input type="radio" name="rating" value="4"><i></i>
+            <input type="radio" name="rating" value="5"><i></i>
+        </span>
         <div class='row'>
             <div class='col-md-6 col-md-offset-3'>
                 <div class='line'></div>
             </div>
         </div>
+
+
     @endforeach
 
 
@@ -76,5 +85,11 @@
             }
 
         });
+
+        $(':radio').change(
+  function(){
+    $('.choice').text( this.value + ' stars' );
+  } 
+)
     </script>
 @stop
