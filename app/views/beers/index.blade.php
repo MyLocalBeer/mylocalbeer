a146fa4946265c5024ec4b1efaa14815c769d9b3
Liz Cole <liz.cole1227@gmail.com>
2015-01-09 15:50:19 -0600

    Finished beer index working on Edit beer page(location drop down not working)

--- app/views/beers/index.blade.php
+++ app/views/beers/index.blade.php
@@ -10,25 +10,34 @@
     @foreach($beers as $beer)
         <div id='breweries'>
             <article>
-                <div class='name-local'>
-                    <div class='row'>
-                        <div class='beerinfo beertitle col-md-6 col-md-offset-3'>
+                <div class='row'>
+                    @if(Entrust::can('can_edit_beer'))
+                    <div class='col-md-2 col-md-offset-8 text-center'>
+                        <a href="{{ action('BeersController@edit', $beer->id) }}"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></a>
+                    @endif
+                    @if(Entrust::can('can_delete_beer'))
+                        <button class='delete_button' data-post-id="{{{$beer->id}}}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
+                    </div>
+                    @endif
+                </div>
+                <div class='row'>
+                        <div class='beertitle col-md-6 col-md-offset-3 text-center'>
                             {{ $beer->beer_name }}
                         </div>
                     </div>
-                </div>
+                
                 <div class='row'>
-                    <div class='col-md-6 col-md-offset-3 beerinfo'>
+                    <div class='col-md-6 col-md-offset-3 beerinfo text-center'>
                         {{ $beer->beer_style }}
                     </div>
                 </div>
                 <div class='row'>    
-                    <div class='col-md-6 col-md-offset-3 breerinfo'>
-                        {{ $beer->abv }}
+                    <div class='col-md-6 col-md-offset-3 beerinfo text-center'>
+                        ABV: {{ $beer->abv }} %
                     </div>
                 </div>
                 <div class='row'>   
-                    <div class='col-md-6 col-md-offset-3 brewery-info abel-font'>
+                    <div class='col-md-6 col-md-offset-3 beerdescription text-center'>
                         {{ $beer->description }}
                     </div>
                 </div> 
@@ -39,21 +48,15 @@
                 </div> 
                 </div>  -->
                 <div class='row'>
-                        @if(Entrust::can('can_edit_beer'))
-                        <div class='col-md-2 col-md-offset-10'>
-                            <a href="{{ action('BeersController@edit', $beer->id) }}"><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></a>
-                        @endif
-                        @if(Entrust::can('can_delete_beer'))
-                            <button class='delete_button' data-post-id="{{{$beer->id}}}"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button>
-                        </div>
-                        @endif
+
               </div>
             </article>
         </div>
 
         <div class='row'>   
             <div class='col-md-6 col-md-offset-3 text-center'>
-               <input id="input-id" type="number" class="rating" min=0 max=5 step=1 data-size="sm" > 
+                     <input name='rating' id="input-id" type="integer" class="rating" min="0" max="5" step="1" data-size="sm" > 
+                    <button id='rating' type='submit' class='btn btn-info btn-sm'>Rate</button>
             </div>
         </div> 
 
